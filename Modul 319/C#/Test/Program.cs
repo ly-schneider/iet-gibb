57533821a91ca0c0d7034e7eb622c92881838f6f
lyschneider <71436505+ly-schneider@users.noreply.github.com>
2024-08-29 09:18:02 +0200

    Add Modul 319

---
+++ Modul 319/C#/Test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int num = 10;
+      Console.WriteLine(num);
+      num = 2147483647;
+      double num2 = 9.92;
+
+      char myletter = 'B';
+      string myletterStr = "B";
+
+      Console.WriteLine(myletter);
+      Console.WriteLine(myletterStr);
+
+      Console.WriteLine(num + num2);
+
+      Console.Write("Hello World! ");
+      Console.Write("I will print on the same line.");
+    }
+  }
+}
